e8757c131983ede0df3fe7b8fe6862b72adcb1aa
Kiera Carman <kiera.carman@gmail.com>
2025-02-22 18:02:12 -0500

    remove menu button

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Menu from '$lib/Menu.svelte';
 	let { data } = $props();
 </script>
 
@@ -8,12 +7,10 @@
 		<h1>404</h1>
 		<p>{data.error.message}</p>
 		<p><small>{data.error.hint}</small></p>
-		<Menu />
 	</div>
 {:else}
-	<article class="">
+	<article>
 		<h1>{data.meta.title}</h1>
-		<div class="recipe-content">{@html data.content}</div>
-		<Menu />
+		<div>{@html data.content}</div>
 	</article>
 {/if}
